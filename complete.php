959e6022d2e087549229e0a8700e4dc68c255c31
Eugene <jonscorpions1@gmail.com>
2025-06-22 12:45:11 +0300

    Added complete.php, added checkbox in index.php

---
+++ complete.php
@@ -0,0 +1,10 @@
+<?php
+require 'config.php';
+
+if (isset($_POST['id'])) {
+    $comleted = isset($_POST['completed']) ? 1 : 0;
+    $stmt = $pdo->prepare("UPDATE tasks SET is_completed = ? WHERE id = ?");
+    $stmt->execute([$comleted, $_POST['id']]);
+}
+header('Location: index.php');
+?>
--- index.php
+++ index.php
@@ -12,7 +12,7 @@
         $stmt = $pdo -> query("SELECT * FROM tasks ORDER BY created_at DESC");
         if ($stmt->rowCount() > 0) {      
             while ($row = $stmt -> fetch()):
-                $tasksClass = $row['is_complited'] ? 'complited' : '';
+                $tasksClass = $row['is_completed'] ? 'completed' : '';
                 ?>
 
                 <div class="task <?= $taskClass ?>">
@@ -20,10 +20,14 @@
                     <p><?= htmlspecialchars($row['description']) ?></p>
                     <small>
                         Дата создания: <?= $row['created_at'] ?> | 
-                        Статус: <?= $row['is_complited'] ? 'Выполнено' : 'В процессе' ?>
+                        Статус: <?= $row['is_completed'] ? 'Выполнено' : 'В процессе' ?>
                     </small>
                     <a href="edit.php?id=<?= $row['id'] ?>">Обновить задачу</a>
                     <a href="delete.php?id=<?= $row['id'] ?>">Удалить задачу</a>
+                    <form action="complete.php" method="POST" style="display:inline;">
+                        <input type="hidden" name="id" value="<?= $row['id'] ?>">
+                        <input type="checkbox" name="completed" onchange="this.form.submit()" <?= $row['is_completed'] ? 'checked' : '' ?>>
+                    </form>
                 </div>
 
                 <?php endwhile; ?>
