67b384b16b66d841fc7de84c2ac75d87c56635be
Eugene <jonscorpions1@gmail.com>
2025-07-22 19:12:59 +0300

    Fixed typo in $completed. Added error handling and checking for task existence in complete.php

--- public/complete.php
+++ public/complete.php
@@ -2,9 +2,19 @@
 require '../config/database.php';
 
 if (isset($_POST['id'])) {
-    $comleted = isset($_POST['completed']) ? 1 : 0;
-    $stmt = $pdo->prepare("UPDATE tasks SET is_completed = ? WHERE id = ?");
-    $stmt->execute([$comleted, $_POST['id']]);
+    $completed = isset($_POST['completed']) ? 1 : 0;
+    
+    try {
+        $stmt = $pdo->prepare("UPDATE tasks SET is_completed = ? WHERE id = ?");
+        $stmt->execute([$completed, $_POST['id']]);
+
+        if ($stmt->rowCount() === 0) {
+            $_SESSION['error'] = 'Задача не найдена';
+        }
+    }
+    catch (PDOException $e) {
+        $_SESSION['error'] = "Ошибка обновления: " . $e->getMessage();
+    }
 }
 header('Location: index.php');
 ?>
