3218e9cf7fc4486cee62d864a5f09824361f2bd3
Eugene <jonscorpions1@gmail.com>
2025-07-25 13:56:09 +0300

    Update database dump. Added tabele users, column 'user_id' in table 'tasks'

--- todo_app-dump.sql
+++ todo_app-dump.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Хост: 127.0.0.1
--- Время создания: Июн 21 2025 г., 09:40
+-- Время создания: Июл 25 2025 г., 12:49
 -- Версия сервера: 10.4.32-MariaDB
 -- Версия PHP: 8.2.12
 
@@ -32,15 +32,37 @@ CREATE TABLE `tasks` (
   `title` varchar(100) NOT NULL,
   `description` text DEFAULT NULL,
   `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
-  `is_complited` tinyint(1) DEFAULT 0
+  `is_completed` tinyint(1) DEFAULT 0,
+  `user_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Дамп данных таблицы `tasks`
 --
 
-INSERT INTO `tasks` (`id`, `title`, `description`, `created_at`, `is_complited`) VALUES
-(1, 'Тестовая задача', 'Описание', '2025-06-20 14:46:48', 0);
+INSERT INTO `tasks` (`id`, `title`, `description`, `created_at`, `is_completed`, `user_id`) VALUES
+(1, 'Тестовая задача (update)', 'Описание (update)', '2025-06-20 14:46:48', 0, 1),
+(3, 'Купить Глеб', 'Глеб это хлеб', '2025-06-22 09:27:23', 0, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `password`, `created_at`) VALUES
+(1, 'test', '$2y$12$jJx59tMVLbyXrp/S8z1WmuSS6tWM9eoGZRVlFUZaqH3O1QpS8XdIW', '2025-07-25 10:28:11');
 
 --
 -- Индексы сохранённых таблиц
@@ -50,7 +72,15 @@ INSERT INTO `tasks` (`id`, `title`, `description`, `created_at`, `is_complited`)
 -- Индексы таблицы `tasks`
 --
 ALTER TABLE `tasks`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `username` (`username`);
 
 --
 -- AUTO_INCREMENT для сохранённых таблиц
@@ -60,7 +90,23 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT для таблицы `tasks`
 --
 ALTER TABLE `tasks`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Ограничения внешнего ключа сохраненных таблиц
+--
+
+--
+-- Ограничения внешнего ключа таблицы `tasks`
+--
+ALTER TABLE `tasks`
+  ADD CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
